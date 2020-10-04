6bd501a8ebf9a20b6e83335c7b8e808b71aee660
JeonHG <jeonhgfm101@gmail.com>
2020-10-04 16:24:35 +0900

    3.2 realtime nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,23 @@
 import React, { useState } from "react";
 import { dbService } from "fbase";
+import { useEffect } from "react";
 
 export default function Home() {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection("nweets").get();
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection("nweets").add({
@@ -29,6 +44,13 @@ export default function Home() {
         />
         <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
